834916e0d8c13229705178f55ee2b5dd4ae6b8ad
Ayush Singh <ayush-_-singh@live.com>
2019-03-13 16:30:29 +0530

    add typing for android, bump version

--- index.d.ts
+++ index.d.ts
@@ -1,7 +1,11 @@
 import * as fs from "tns-core-modules/file-system";
 
 export declare class DownloadProgress {
-    constructor();
-    addProgressCallback(callback: any);
-    downloadFile(url: string, destinationFilePath?: string): Promise<fs.File>;
+  constructor();
+  addProgressCallback(callback: any);
+  downloadFile(
+    url: string,
+    options: any,
+    destinationFilePath?: string
+  ): Promise<fs.File>;
 }
