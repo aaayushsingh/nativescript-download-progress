b88ee48ef11f28812f576337e5e90bec18a87455
Ayush Singh <ayush-_-singh@live.com>
2019-03-13 15:50:07 +0530

    add optional headers param to download-progress.android

--- download-progress.android.ts
+++ download-progress.android.ts
@@ -2,47 +2,57 @@ import * as application from "tns-core-modules/application";
 import * as fs from "tns-core-modules/file-system";
 
 export class DownloadProgress {
+  private promiseResolve;
+  private promiseReject;
+  private progressCallback;
 
-    private promiseResolve;
-    private promiseReject;
-    private progressCallback;
+  public addProgressCallback(callback: any) {
+    this.progressCallback = callback;
+  }
 
-    public addProgressCallback(callback: any) {
-        this.progressCallback = callback;
+  public downloadFile(
+    url: string,
+    headers?: any,
+    destinationFilePath?: string
+  ): Promise<fs.File> {
+    var worker;
+    if (global["TNS_WEBPACK"]) {
+      var WorkerScript = require("nativescript-worker-loader!./android-worker.js");
+      worker = new WorkerScript();
+    } else {
+      worker = new Worker("./android-worker.js");
     }
+    return new Promise<fs.File>((resolve, reject) => {
+      this.promiseResolve = resolve;
+      this.promiseReject = reject;
 
-    public downloadFile(url: string, destinationFilePath?: string): Promise<fs.File> {
-        var worker;
-        if (global["TNS_WEBPACK"]) {
-            var WorkerScript = require("nativescript-worker-loader!./android-worker.js");
-            worker = new WorkerScript();
+      worker.postMessage({
+        url,
+        headers: headers,
+        destinationFilePath: destinationFilePath
+      });
+      worker.onmessage = (msg: any) => {
+        if (msg.data.progress) {
+          if (this.progressCallback) {
+            this.progressCallback(msg.data.progress);
+          }
+        } else if (msg.data.filePath) {
+          worker.terminate();
+          this.promiseResolve(fs.File.fromPath(msg.data.filePath));
+        } else {
+          worker.terminate();
+          this.promiseReject(msg.data.error);
         }
-        else {
-            worker = new Worker("./android-worker.js");
-        }
-        return new Promise<fs.File>((resolve, reject) => {
-            this.promiseResolve = resolve;
-            this.promiseReject = reject;
+      };
 
-            worker.postMessage({ url: url, destinationFilePath: destinationFilePath });
-            worker.onmessage = (msg:any)=> {
-                if(msg.data.progress) {
-                    if(this.progressCallback) {
-                        this.progressCallback(msg.data.progress);
-                    }
-                } else if(msg.data.filePath) {
-                    worker.terminate();
-                    this.promiseResolve(fs.File.fromPath(msg.data.filePath));
-                } else {
-                    worker.terminate();
-                    this.promiseReject(msg.data.error);
-                }
-            }
-        
-            worker.onerror = (err)=> {
-                console.log(`An unhandled error occurred in worker: ${err.filename}, line: ${err.lineno} :`);
-                this.promiseReject(err.message);
-            }
-        });
-    }
+      worker.onerror = err => {
+        console.log(
+          `An unhandled error occurred in worker: ${err.filename}, line: ${
+            err.lineno
+          } :`
+        );
+        this.promiseReject(err.message);
+      };
+    });
+  }
 }
